0c02f1b6d7b25f078d4206c306c73a066a0d6dcc
BrunoDM2943 <bdm2943@gmail.com>
2018-10-13 20:35:39 -0300

    init login

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const utils = require("./app/utils");
 const movements = require("./app/movements");
+const users = require("./app/users");
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
@@ -16,6 +17,8 @@ app.get("/movements", movements.list);
 app.get("/movements/:id", movements.get);
 app.put("/movements/:id", movements.update);
 app.delete("/movements/:id", movements.remove);
+app.post("/users", users.signUp);
+app.post("/users/login", users.login);
 
 const PORT = process.env.PORT || 3000;
 
